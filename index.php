ccbceb1e24925cae788b70695efc883b2eb1d9eb
dhimanpaulchowdhury <dhimanpaulshowdhury@hotmail.com>
2015-09-06 23:29:46 +0530

    New changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo nl2br("Hello there");
+echo nl2br("Hello there!");
 echo nl2br("Power shell is fun and smooth");
 echo nl2br(" This is a demo for php site hosting in azure using poweshell.\n\n");
 phpinfo();
